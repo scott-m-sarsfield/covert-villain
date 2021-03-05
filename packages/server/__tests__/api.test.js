8128c85ee5926a46e368a8bd114b9362b1e92aa2
Scott M Sarsfield <scott.m.sarsfield@gmail.com>
2021-03-04 19:36:56 -0800

    feat: auto-restart old rooms
    - i.e. when joining a room that hasn't been updated in the past hour, reset the room.

--- packages/server/__tests__/api.test.js
+++ packages/server/__tests__/api.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const express = require('express');
-const { sequelize } = require('../models');
+const { sub } = require('date-fns');
+const { sequelize, Game } = require('../models');
 const api = require('../api');
 
 const app = express();
@@ -21,11 +22,11 @@ beforeEach(() => {
 });
 
 describe('/games/join', () => {
-  describe('when jwt is missing', () => {
-    function post(url, params) {
-      return request(app).post(url).send(params);
-    }
+  function post(url, params) {
+    return request(app).post(url).send(params);
+  }
 
+  describe('when jwt is missing', () => {
     it('adds the player to the game and sends a signal to refresh the room', async () => {
       const response = await post('/api/games/join', { name: 'Alpha', code: 'abxgyp' });
 
@@ -47,6 +48,58 @@ describe('/games/join', () => {
       });
 
       expect(sendRoomEvent).toHaveBeenCalledWith('ABXGYP', 'refresh');
+    });
+  });
+
+  describe('when trying to join an active game', () => {
+    it('should deny the user from joining', async () => {
+      await Game.create({
+        code: 'ABXGYP',
+        data: {
+          phase: 'president_chooses_chancellor'
+        },
+        updatedAt: sub(new Date(), { minutes: 58 }),
+        createdAt: sub(new Date(), { days: 1 })
+      }, { silent: true });
+
+      const response = await post('/api/games/join', { name: 'Alpha', code: 'ABXGYP' });
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toEqual({
+        error: 'cannot join game in progress'
+      });
+    });
+  });
+
+  describe('when a game with the same code has been abandoned', () => {
+    it('should reset the game with the joining player as host', async () => {
+      await Game.create({
+        code: 'ABXGYP',
+        data: {
+          phase: 'president_chooses_chancellor'
+        },
+        updatedAt: sub(new Date(), { hours: 2 }),
+        createdAt: sub(new Date(), { days: 1 })
+      }, { silent: true });
+
+      const response = await post('/api/games/join', { name: 'Alpha', code: 'ABXGYP' });
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual({
+        game: expect.objectContaining({
+          players: expect.arrayContaining([
+            expect.objectContaining({
+              name: 'Alpha',
+              uuid: expect.anything()
+            })
+          ])
+        }),
+        user: expect.objectContaining({
+          name: 'Alpha',
+          roomCode: 'ABXGYP'
+        }),
+        token: expect.anything()
+      });
     });
   });
 });
